318430c42b56df2261dd2c0d83d44effb700bcce
vsanjuan <salvador@aceleratuempresa.net>
2016-04-09 01:42:59 +0200

    feat initial todo_app basic menu

---
+++ __openerp__.py
@@ -0,0 +1,8 @@
+{
+	'name': 'To-do Application',
+	'description': 'Manage your personal Tasks',
+	'author': 'Salvador',
+	'depends': ['mail'],
+	'data': ['todo_view.xml'],
+	'application': True,
+}
---
+++ todo_model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+class TodoTask(models.Model):
+	_name = 'todo.task'
+	name = fields.Char('Description', required =True)
+	is_done = fields.Boolean('Done?')
+	active = fields.Boolean('Active?',default=True)
