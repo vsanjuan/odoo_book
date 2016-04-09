81135da7da197c9e60871c9dee020503d5bd0687
vsanjuan <salvador@aceleratuempresa.net>
2016-04-09 12:27:30 +0200

    feat add security access to task only for user created

--- __openerp__.py
+++ __openerp__.py
@@ -5,7 +5,8 @@
 	'depends': ['mail'],
 	'data': [
 		'todo_view.xml',
-		'security/ir.model.access.csv'
+		'security/ir.model.access.csv',
+		'security/todo_access_rules.xml',
 		],
 	'application': True,
 }
