f404d5262b07183ace8bf3294d0dd099d6a4e7f2
vsanjuan <salvador@aceleratuempresa.net>
2016-04-09 12:15:55 +0200

    feat added security permissions

--- __openerp__.py
+++ __openerp__.py
@@ -3,6 +3,9 @@
 	'description': 'Manage your personal Tasks',
 	'author': 'Salvador',
 	'depends': ['mail'],
-	'data': ['todo_view.xml'],
+	'data': [
+		'todo_view.xml',
+		'security/ir.model.access.csv'
+		],
 	'application': True,
 }
